f315cfc953bf2373402769c1a53e4e9fc87a1d3d
dakufify <military.ninja@ya.ru>
2025-05-12 13:43:32 +0300

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+load_dotenv()  # загружаем переменные из .env
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Привет! Я бот на aiogram!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
