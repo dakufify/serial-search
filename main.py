98c9334203c400d4a194fa5b4d0806ac7dd66f91
dakufify <military.ninja@ya.ru>
2025-05-13 17:28:14 +0300

    venv

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import os
+import os,sys
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import Message
 from dotenv import load_dotenv
+
+activate_this = '/home/a1126610/bot_venv/bin/activate_this.py'
+with open(activate_this) as f:
+    exec(f.read(), {'__file__': activate_this})
 
 load_dotenv()  # загружаем переменные из .env test_git
 
